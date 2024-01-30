e8ac66278ce1bf80f9f657857f4924553364d4bb
Justin Derot <justinderot@hotmail.ca>
2024-01-29 17:55:24 -0800

    Revert "Merge Attempt with Animations"
    This reverts commit 8047b33e74edc29b0ed2ece3ca2b875295d8c0d7.

--- Assets/Scripts/Enemies/Exploder.cs
+++ Assets/Scripts/Enemies/Exploder.cs
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -54,69 +53,3 @@ public class Exploder : Enemy
 
 
 }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Exploder : Enemy
-{
-    private float explodeRadius = 1f;
-    private float damage;
-
-    [SerializeField] private Animator anim;
-
-    protected override void Start()
-    {
-        base.Start();
-        SetEnemyType(EnemyType.Exploder);
-        health = new Health(1, 1, 0);
-    }
-
-    protected override void Update()
-    {
-        base.Update();
-        if (target == null)
-        {
-            return;
-        }
-        
-        if (Vector2.Distance(transform.position, target.position) <= explodeRadius)
-        {
-            Explode(explodeRadius);
-        }
-        
-        
-    }
-
-    public void Explode(float radius)
-    {
-        //Debug.Log($"explode with radius {radius}");
-        
-        target.GetComponent<IDamageable>().GetDamage(damage);
-        Destroy(this.gameObject, 0.6f);
-        anim.SetBool("isDead", true);
-    }
-    public override void GetDamage(float damage)
-    {
-        base.GetDamage(damage);
-    }
-
-    public void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Block"))
-        {
-            collision.gameObject.GetComponent<IDamageable>().GetDamage(damage);
-            Destroy(gameObject);
-        }
-    }
-    public void SetExploder(float _explodeRadius, float _damage)
-    {
-        this.explodeRadius = _explodeRadius;
-        this.damage = _damage;
-    }
-
-
-
-}
->>>>>>> Stashed changes
